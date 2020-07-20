e9ed7e840d04d65ef27293bc9d7ea57469b28b5d
Kyler Mintah <kylermintah@gmail.com>
2020-07-20 17:10:03 -0400

    small tweaks

--- events/models/events.model.js
+++ events/models/events.model.js
@@ -2,7 +2,6 @@ const mongoose = require('../../common/services/mongoose.service').mongoose;
 const Schema = mongoose.Schema;
 
 const venueSchema = new Schema ({
-    id: String,
     name: String,
     city: String,
     country: String,
@@ -15,7 +14,6 @@ const venueSchema = new Schema ({
 });
 
 const eventManagerSchema = new Schema ({
-    id: String,
     event_id: String,
     users_attended: [String],
     left_swiped_users: [String],
@@ -24,7 +22,6 @@ const eventManagerSchema = new Schema ({
 });
 
 const eventSchema = new Schema({
-    id: String, //TODO: overwrite _id?
     name: String,
     details: String,
     start_date: Date, //TODO: timestamp?
