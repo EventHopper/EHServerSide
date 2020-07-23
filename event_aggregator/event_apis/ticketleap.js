5191e77b8a78159988394440ec13aa9b973ba81f
Batchema Sombie <>
2020-07-23 17:40:48 -0400

    schema updated. Solves #36

--- event_aggregator/event_apis/ticketleap.js
+++ event_aggregator/event_apis/ticketleap.js
@@ -2,14 +2,12 @@ const axios = require("axios");
 const constants = require("../apiconstants");
 const config = require("../../common/config/env.config");
 const eventModel = require("../../events/models/events.model.js");
-
-
-/***************************************************************************//**
+/**
  * EXTERNAL VENDOR API (EVAPI) Integration
  * @host Ticketleap
  * @author Kyler Mintah
  * @module event_aggregator
- * 
+ *
  * REQUIRED FUNCTIONS
  * @function aggregateExternalVendor returns vendor object
  * @function importToDatabase saves aggregated events to databse
@@ -17,7 +15,7 @@ const eventModel = require("../../events/models/events.model.js");
  * @param location EventHopper location object
  ******************************************************************************/
 
-exports.aggregateExternalVendor = aggregateExternalVendor;
+/***************************************************************************/ exports.aggregateExternalVendor = aggregateExternalVendor;
 
 function aggregateExternalVendor(location) {
   //Construct URL
@@ -27,7 +25,7 @@ function aggregateExternalVendor(location) {
   var month = now.getMonth() + 1; //January is 0
   var day = now.getDate();
   var date = year.toString() + "-" + month.toString() + "-" + day.toString();
-  let page_num = 1; 
+  let page_num = 1;
 
   const api_url =
     constants.ticketleapURL +
@@ -42,8 +40,11 @@ function aggregateExternalVendor(location) {
     date +
     "&page_num=";
 
+  console.log(api_url);
+
   //send http request
-  axios.get(api_url + page_num) //TODO: keep iterating the page numbers till the events array is empty
+  axios
+    .get(api_url + page_num) //TODO: keep iterating the page numbers till the events array is empty
     .then((response) => {
       var events = response.data.events; //array of event objects
       importToDatabase(events);
@@ -51,22 +52,17 @@ function aggregateExternalVendor(location) {
     .catch((error) => {
       console.log(error);
     });
-};
+}
 
 function importToDatabase(external_events) {
   external_events.forEach((element) => {
     var newEvent = {
       name: element.name,
       details: element.description,
-      start_date: element.earliest_start_utc, //TODO: timestamp? probably not according to what I read, timestamp not needed here. Date.parse() gives timestamp though
-      end_date: element.earliest_end_utc,
-      date_created: element.created_utc,
-      expiry_date: element.latest_end_utc,
-      creator_iD: null, //TODO: remove?
-      organizer: {
-        id: null, //TODO: Do we need this? Are we storing external organizers?
-        name: element.organization_name,
-      },
+      start_date_utc: element.earliest_start_utc,
+      end_date_utc: element.latest_end_utc,
+      source: "TL", //ticketLeap code
+      organizer: element.organization_name,
       venue: {
         name: element.venue_name,
         city: element.venue_city,
@@ -77,19 +73,15 @@ function importToDatabase(external_events) {
         location: {
           latitude: null,
           longitude: null,
+          timezone: element.venue_timezone,
         },
       },
-      type: null, //FIXME: Need to add
-      categories: element.hashtag_text ? element.hashtag_text.split(" ") : null, // TODO: Discuss
-      status: ["upcoming", "past", "cancelled"], //FIXME: Unsure of ticket leap equivalent
-      rsvp_required: null, //FIXME: Unsure of ticket leap equivalent
-      image_url:
-        element.hero_image_url ||
-        element.hero_small_image_url ||
-        element.image_url_full, //ACK: hero images never null
+      category: null, //FIXME: Need to add
+      tags: element.hashtag_text ? element.hashtag_text.split(" ") : null, // TODO: Discuss
+      element_url_full: element.image_url_full,
+      image_url_small: element.hero_image_url || element.hero_small_image_url,
       public_action: element.url,
       event_manager_id: null, //FIXME: Unsure of ticket leap equivalent
-      permissionLevel: null, //FIXME: Unsure of ticket leap equivalent
     };
 
     console.log(newEvent);
--- events/index.js
+++
@@ -1,3 +0,0 @@
-export * as EventModel from './models/events.model';
-export * as EventManager from './models/events_manager.model';
-export * as EventController from './controllers/events.controller';
