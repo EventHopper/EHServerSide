340a4456d7422352c1f4b1d07bd96ac9224e9f79
Kyler Mintah <kylermintah@gmail.com>
2020-08-02 17:34:50 -0400

    update

--- services/aggregator/aggregator.js
+++ services/aggregator/aggregator.js
@@ -1,16 +1,18 @@
-const ticketLeap = require("./event_apis/ticketleap");
-const ticketMaster = require("./event_apis/ticketmaster");
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+const ticketLeap = require('./event_apis/ticketleap');
+const ticketMaster = require('./event_apis/ticketmaster');
 
-/***************************************************************************//**
+/** *************************************************************************//**
  * EVENT AGGREGATOR
  *
  * The Event Aggregator batch updates external events provided by vendor APIs
  * @function aggregate imports external events into event database
  ******************************************************************************/
 
-function aggregate(location){ 
-    ticketLeap.aggregateExternalVendor(location);
-    ticketMaster.aggregateExternalVendor(location);
+function aggregate(location) {
+  ticketLeap.aggregateExternalVendor(location);
+  ticketMaster.aggregateExternalVendor(location);
 }
 
 exports.aggregate = aggregate;
--- services/aggregator/settings.js
+++ services/aggregator/settings.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    LOGGING : false,
-    SOFT_LOGGING : false,
-    VERBOSE_LOGGING : false
+  LOGGING: false,
+  SOFT_LOGGING: false,
+  VERBOSE_LOGGING: false,
 });
