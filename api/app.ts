148bdcdc50b0df81a1d02f27d94ce5ebb4d68694
Ransford Antwi <ransford@seas.upenn.edu>
2020-08-04 22:05:49 -0400

    created authmiddleware

--- api/app.ts
+++ api/app.ts
@@ -1,22 +1,46 @@
 /* eslint-disable require-jsdoc */
 import express from 'express';
 import * as bodyParser from 'body-parser';
+import Auth from '../auth/authentication';
+import Realm from 'realm';
 
 
 class App {
   public app: express.Application;
   public port: number;
+  private auth: Auth;
+  private user: Realm.User;
 
   constructor(controllers:any, port:number) {
     this.app = express();
     this.port = port;
+    this.auth = new Auth();
 
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
   }
 
+  private async authMiddleware(request: express.Request,
+      response: express.Response, next:express.NextFunction) {
+    console.log(`${request.method} ${request.path}`);
+    // if (this.tokenCache === '') {
+    //   const apikey:string = request.body.key;
+    //   this.tokenCache = await this.auth.loginApiKey(apikey);
+    //   // eslint-disable-next-line max-len
+    //   this.tokenCache === '' ? next() : response.send('Could not authenticate user');
+    // } else {
+    //   next();
+    // }
+
+    /* loginApiKey('frfhr').then((user) => {
+  user ? console.log('alive') : console.log('nah');
+  // console.log('Successfully logged in!', user);
+}); */
+  }
+
   private initializeMiddlewares() {
     this.app.use(bodyParser.json());
+    this.app.use(this.authMiddleware);
   }
 
   private initializeControllers(controllers:any) {
--- auth/authentication.ts
+++ auth/authentication.ts
@@ -1,36 +1,41 @@
+/* eslint-disable require-jsdoc */
 /* eslint-disable max-len */
 import Realm from 'realm';
 import assert from 'assert';
 import {REALM_APP_ID as ID} from '../common/utils/config';
 
-const app = new Realm.App({id: `${ID}`}); // TODO: change to dotenv
+class Auth {
+   private app:Realm.App = new Realm.App({id: `${ID}`});
+   public authUser:Realm.User;
 
-/**
+   constructor() {
+     this.authUser = new Realm.User();
+   }
+   /**
  * Logs in a user onto the Realm App via and APIKey.
  * To log in with an API key, create an API Key credential with a server
  * or user API key and pass it to App.logIn()
  * @param {string} apiKey The API key.
  * @return {int} The sum of the two numbers.
  */
-async function loginApiKey(apiKey: string) {
-  // Create an API Key credential
-  console.log('hi');
-  const credentials = Realm.Credentials.serverApiKey(apiKey);
+   public async loginApiKey(apiKey: string) {
+     // Create an API Key credential
+     console.log('hi');
+     const credentials = Realm.Credentials.serverApiKey(apiKey);
 
-  try {
-    // Authenticate the user
-    const user: Realm.User = await app.logIn(credentials);
+     try {
+       // Authenticate the user
+       const user: Realm.User = await this.app.logIn(credentials);
 
-    // `App.currentUser` updates to match the logged in user
-    assert(user.id === app.currentUser!.id);
-
-    return user;
-  } catch (err) {
-    console.error('Failed to log in', err);
-  }
+       // `App.currentUser` updates to match the logged in user
+       assert(user.id === this.app.currentUser!.id);
+       this.authUser = user;
+       return this.authUser;
+     } catch (err) {
+       console.error('Failed to log in', err);
+       return null;
+     }
+   }
 }
 
-loginApiKey('frfhr').then((user) => {
-  user ? console.log('alive') : console.log('nah');
-  // console.log('Successfully logged in!', user);
-});
+export default Auth;
