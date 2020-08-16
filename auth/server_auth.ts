eb91d901fc69a6eaa56dbc3ae5419bfffdabf04e
Batchema Sombie <>
2020-08-16 15:54:59 -0400

    merging

--- auth/server_auth.ts
+++ auth/server_auth.ts
@@ -6,58 +6,7 @@ import assert from 'assert';
 import {REALM_APP_ID as ID} from '../common/utils/config';
 
 class Auth {
-<<<<<<< HEAD
-   public app:Realm.App = new Realm.App({id: `${ID}`});
-   public authUser!: Realm.User;
-
-
-   public getInstance():Realm.App {
-     return this.app;
-   }
-
-   public getAccessToken() {
-     //  let realm = new Realm({schema: []});
-     //  realm.write
-     return this.authUser.accessToken;
-   }
-
-   public refreshToken() {
-     return this.authUser.refreshToken;
-   }
-
-   public hasAccessToken() {
-     if (!this.authUser) {
-       return false;
-     }
-     this.authUser.refreshToken;
-     return this.authUser != null;
-   }
-
-   /**
-   * Logs in a user onto the Realm App via and APIKey.
-   * To log in with an API key, create an API Key credential with a server
-   * or user API key and pass it to App.logIn()
-   * @param {string} apiKey The API key.
-   * @return {int} The sum of the two numbers.
-   */
-   public loginApiKey = async (apiKey: string) => {
-     // Create an API Key credential
-     const credentials = Realm.Credentials.serverApiKey(apiKey);
-
-     try {
-       // Authenticate the user
-       const user: Realm.User = await this.app.logIn(credentials);
-
-       // `App.currentUser` updates to match the logged in user
-       assert(user.id === this.app.currentUser!.id);
-       this.authUser = user;
-       return 'AUTH_SUCCESS';
-     } catch (err) {
-       return 'AUTH_FAILED';
-     }
-   }
-=======
-  private app: Realm.App = new Realm.App({id: `${ID}`});
+  public app: Realm.App = new Realm.App({id: `${ID}`});
   public authUser!: Realm.User;
 
 
@@ -84,7 +33,7 @@ class Auth {
   * @param {string} apiKey The API key.
   * @return {int} The sum of the two numbers.
   */
-  public loginApiKey = async (apiKey: string) => {
+  public loginApiKey = async(apiKey: string) => {
     // Create an API Key credential
     const credentials = Realm.Credentials.serverApiKey(apiKey);
 
@@ -100,7 +49,6 @@ class Auth {
       return 'AUTH_FAILED';
     }
   }
->>>>>>> batchema/typescript
 }
 
 export default Auth;
