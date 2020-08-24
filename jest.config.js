0b756da902a2303fc59e8c621aa182920c71bb6d
Kyler Mintah <kylermintah@gmail.com>
2020-08-23 22:41:28 -0400

    quick fix

--- jest.config.js
+++ jest.config.js
@@ -40,14 +40,14 @@ module.exports = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  "coverageThreshold": {
-    "global": {
-      "branches": 90,
-      "functions": 90,
-      "lines": 90,
-      "statements": -10
+  'coverageThreshold': {
+    'global': {
+      'branches': 90,
+      'functions': 90,
+      'lines': 90,
+      'statements': -10
     }
-  }
+  },
   
 
   // A path to a custom dependency extractor
@@ -77,14 +77,14 @@ module.exports = {
   // ],
 
   // An array of file extensions your modules use
-    moduleFileExtensions: [
-      "js",
-      "json",
-      "jsx",
-      "ts",
-      "tsx",
-      "node"
-    ],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'ts',
+    'tsx',
+    'node'
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
