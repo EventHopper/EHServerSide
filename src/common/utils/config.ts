8193a1baf9fc292c134095755130d1fd0d6f98b2
Kyler Mintah <kylermintah@gmail.com>
2020-12-10 23:52:20 -0500

    Solving Invalid PEM formatted message Error

--- src/common/utils/config.ts
+++ src/common/utils/config.ts
@@ -51,16 +51,16 @@ export const variables = {
     },
     firebase: {
       serviceAccountObject: {
-        'type': String(process.env.FSAV_TYPE),
-        'project_id': String(process.env.FSAV_PROJECT_ID),
-        'private_key_id': String(process.env.FSAV_PRIVATE_KEY_ID),
-        'private_key': String(process.env.FSAV_PRIVATE_KEY).replace(/\\n/g, '\n'),
-        'client_email': String(process.env.FSAV_CLIENT_EMAIL),
-        'client_id': String(process.env.FSAV_CLIENT_ID),
-        'auth_uri': String(process.env.FSAV_AUTH_URI),
-        'token_uri': String(process.env.FSAV_TOKEN_URI),
-        'auth_provider_x509_cert_url': String(process.env.FSAV_AUTH_PROVIDER_X509_CERT_URL),
-        'client_x509_cert_url': String(process.env.FSAV_CLIENT_X509_CERT_URL)
+        'type': process.env.FSAV_TYPE,
+        'project_id': process.env.FSAV_PROJECT_ID,
+        'private_key_id': process.env.FSAV_PRIVATE_KEY_ID,
+        'private_key': process.env.FSAV_PRIVATE_KEY,
+        'client_email': process.env.FSAV_CLIENT_EMAIL,
+        'client_id': process.env.FSAV_CLIENT_ID,
+        'auth_uri': process.env.FSAV_AUTH_URI,
+        'token_uri': process.env.FSAV_TOKEN_URI,
+        'auth_provider_x509_cert_url': process.env.FSAV_AUTH_PROVIDER_X509_CERT_URL,
+        'client_x509_cert_url':process.env.FSAV_CLIENT_X509_CERT_URL
       },
     }
   },
