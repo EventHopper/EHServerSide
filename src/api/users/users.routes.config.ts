58246d901dba841c1749bb31cd12b41b37a385a0
Kyler Mintah <kylermintah@gmail.com>
2021-03-07 00:35:18 -0500

    'Update User' Relationship Function Complete
    - The function in user_relationship.modes.ts updates user relationship state for a given recipient/requester combo
      or relationship id
    - Validates the existance of both users before execution
    - Robust error handling
    - Note: updated the term 'status' to 'state' in user relationship model
      to avoid confusion with server response status-related variables.
    - Added user.validation.ts for server-side sanity checking for use in
      user-related functions. Added 'isValidUser' sanity check that checks
      for the existance of a user.

--- src/api/users/users.routes.config.ts
+++ src/api/users/users.routes.config.ts
@@ -14,6 +14,7 @@ class UserRoutes {
     static userManager = `${UserRoutes.rootPath}/manager/:user_id/:list_type`;
     static userOAuthGrant = `${UserRoutes.rootPath}/:user_id/oauth/grant`;
     static userOAuthRevoke = `${UserRoutes.rootPath}/:user_id/oauth/revoke`;
+    static userRelationshipUpdate = `${UserRoutes.rootPath}/relationships/update`;
 };
 
 export default UserRoutes;
