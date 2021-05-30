fcace88d11f0d4262bd10076aa5452ffcc3f4646
Ransford Antwi <ransford@seas.upenn.edu>
2021-05-30 15:26:13 -0400

    no red

--- src/models/users/event_invite.model.ts
+++ src/models/users/event_invite.model.ts
@@ -25,7 +25,7 @@ const rsvpSchema = new Schema ({
 });
 
 export interface IEventInvite extends Partial<Document> {
-  event_id: String;
+  vendor_id: String;
   requester_id: String;
   rsvp: IRSVP;
 }
@@ -74,161 +74,40 @@ export const EventInvite = userMongoose.model('EventInvites', EventInviteSchema)
 
 /****************************************************************************//**
  * @summary updates a user's rsvp state
- * @description updates an event invite doc
- * @param {requester_id} string id of user who initiated the invite.
+ * @description updates an event invite doc to accept or decline an invite
  * @param {recipient_id} string id of user who's status we're updating
  * @param {state} string the rsvp state of the recipient
  * @param {event_invite_id} string the id of the event invite doc
  * @return returns operation success result status
  * 
  * ****************************************************************************/
-export async function updateEventInviteRsvp(requester_id:string, recipient_id:string, state:number, authenticated_user_id:string, event_invite_id:string) { 
+export async function updateEventInviteRsvp(recipient_id:string, state:number, authenticated_user_id:string, event_invite_id:string) { 
 
-  //Check that only the subjects of the relationship document can edit that document.
-  if(!(authenticated_user_id == requester_id || authenticated_user_id == recipient_id || authenticated_user_id == 'Fordo')){
+  //Check that only the invitee can accept or decline an invite.
+  if(authenticated_user_id != recipient_id){
     return {status: 401, userDoc: {}, message: 'Unauthorized user'}; 
-  }
-  if (!(await isValidUser(requester_id))) {
-    return {status: 404, userDoc: {}, message: 'Requester Does Not Exist.'}; 
   }
   if (!(await isValidUser(recipient_id))){ 
     return {status: 404, userDoc: {}, message: 'Recipient Does Not Exist.'}; 
   }
 
-  if(requester_id == authenticated_user_id && state == 2){
-    return {status: 401, userDoc: {}, message: 'Requestor cannot accept an invite on behalf of someone'}; 
-  }
-  // Check if event invite exists
-
-  var entry = {'recipient_id' : recipient_id, 'state' : state};
-  let eventUpdateResult =  await new Promise<any>((resolve) => { 
-    EventInvite.findOneAndUpdate(
-      {_id: event_invite_id
-      },
-      {useFindAndModify: false, new: true},
-    ).then((userRelationshipDoc:any)  => {
-      debug(userRelationshipDoc);
-      if (userRelationshipDoc != null) {
-        resolve({status: 200, userDoc: userRelationshipDoc, message: 'User Relationship Succesfully Updated.'});
-      } else {
-        resolve({status: 404, userDoc: {}, message: 'User Relationship Does Not Exist.'});
-      }
-    }).catch((err)=>{
-      return {status: -1, message: 'An error occurred during relationship update:\n '+ err};
-    })
-  }).catch((err)=>{
-    return {status: -1, message: 'An error occurred during relationship update:\n '+ err};
-  })
-
-  debug('The result was ' + relationshipUpdateResult.status +' & state was ' +state);
-
-  // If the status of the update is a 0 (which is considered a request rejection) then delete the relationship document
-  if (relationshipUpdateResult.status == 200 && state == 0) {
-    const document_id = relationshipUpdateResult.userDoc._id;
-    debug(await UserRelationship.deleteOne({_id: document_id}, {}, ((err)=>{
-      debug(err);
-    })).then(async ()=> {
-      // Once deleted, we must remove the relationship from the list of each user
-      const removalQuery = {$pull : {relationships: String(document_id)}};
-      await UserModel.updateUser('', removalQuery, requester_id);
-      await UserModel.updateUser('', removalQuery, recipient_id);
-    }).catch((err)=>{
-      return {status: -2, message: 'An error occurred during relationship deletion:\n '+ err};
-    }));
-    
-    return {status: 2, message: 'successfully deleted relationship'};
-
-  }
-  
-  // If relationship does not already exist, we create one
-
-  if (( relationshipUpdateResult.status) == 404 && state != 0) {
-
-    var userRelationshipDocument : IUserRelationship =  {
-      requester_id: requester_id,
-      recipient_id: recipient_id,
-      state: state,
+  let eventInviteUpdateResult =  await new Promise<any>((resolve) => { EventInvite.findOneAndUpdate({_id: event_invite_id, rsvp: {$elemMatch: {recipient_id: recipient_id}}},
+    {$set: {'rsvp.$.state': state,}},
+  ).then((eventInviteDoc:any)  => {
+    debug(eventInviteDoc);
+    if (eventInviteDoc != null) {
+      resolve({status: 200, eventInviteDoc: eventInviteDoc, message: 'Event Invite Doc Succesfully Updated.'});
+    } else {
+      resolve({status: 404, eventInviteDoc: {}, message: 'Event Invite Doc Does Not Exist.'});
     }
-    
-    var doc = new UserRelationship(userRelationshipDocument);
-    let error;
-    let result:any = await doc.save().catch((err)=>{
-      error = {status: -2, message: 'an error occurred during relationship creation:\n'+ err};
-    });
+  }).catch((err)=>{
+    return {status: -1, message: 'An error occurred during event invite update:\n '+ err};
+  }).catch((err)=>{
+    return {status: -1, message: 'An error occurred during event invite update:\n '+ err};
+  });
 
-    if (error) return error;
+  debug('The result was ' + eventInviteUpdateResult.status +' & state was ' +state);
 
-    // console.log(JSON.stringify(result));
-
-    const update = {$addToSet : {relationships: String(result._id)}};
-
-    await UserModel.updateUser('', update, requester_id);
-    await UserModel.updateUser('', update, recipient_id);
-
-    return {status: 2, message: 'successfully created relationship'};
-
-  }
-  
   return {status: 1, message: 'successfully updated relationship'};
-
-};
-
-/****************************************************************************//**
- * @summary gets a list of user relationships for a given user_id
- * @description updates a user relationship data through 
- * @param {requester_id} string id of user who requested relationship
- * @param {recipient_id} string id of user who recieved relationship request
- * @param {state} string user relationship state code
- * @return returns operation success result status
- * 
- * ****************************************************************************/
-export async function getUserRelationshipList(user_id:string, state:number) { // saves to database
-
-  let userRelationshipIDs = (await UserModel.getUserData(undefined,undefined,user_id))['relationships'];
-
-  let userRelationshipList = (await UserRelationship.find({
-    $and:
-     [ 
-       {'_id' : {'$in' : userRelationshipIDs}}, 
-       {state: state},
-     ]
-  })
-    .populate('recipient', 'full_name image_url username')
-    .populate('requester', 'full_name image_url username')
-    .exec()
-    .catch());
-
-  debug(userRelationshipList)
-
-  if (userRelationshipList) return {status: 200, relationship_list: userRelationshipList}
-  else return {status: 400, relationship_list: null}
-
-}
-
-/****************************************************************************//**
- * @summary gets a user relationships for a given two users
- * @description updates a user relationship data through 
- * @param {userid_a} string id of user 1
- * @param {userid_b} string id of user 2
- * @return returns operation success result status
- * 
- * ****************************************************************************/
-export async function getUserRelationship(userid_a:string, userid_b:string) { // saves to database
-
-  let userRelationship = (await UserRelationship.find({
-    $or: 
-        [
-          { requester_id: userid_a, recipient_id: userid_b }, // This or....
-          { requester_id: userid_b, recipient_id: userid_a }, // The inverse
-        ]
-     
-  })
-    .exec()
-    .catch());
-
-  // console.log(userRelationship)
-
-  if (userRelationship) return {status: 200, relationship: userRelationship}
-  else return {status: 400, relationship: null}
-
+  });
 }
